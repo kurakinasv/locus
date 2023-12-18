822fc2d1b1696a6a9598b2acc20bb7ec7dd4eddf
kurakinasv <s.kurakina@ktsstudio.ru>
2023-12-18 03:00:07 +0300

    initial

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Main from './pages/Main';
+
+import './App.css';
+
+const App = () => {
+  return (
+    <>
+      <Main />
+    </>
+  );
+};
+
+export default App;
---
+++ src/pages/Main/Main.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react';
+
+const Main: FC = () => {
+  return (
+    <div>
+      <h1>Main</h1>
+    </div>
+  );
+};
+
+export default Main;
