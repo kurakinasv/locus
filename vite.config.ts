c69e1dde474c504bf3e90625986028f4727c26e8
kurakinasv <kurakinasvv@gmail.com>
2024-06-11 21:46:12 +0300

    [+] pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import react from '@vitejs/plugin-react';
 import autoprefixer from 'autoprefixer';
-import { defineConfig } from 'vite';
+import { ConfigEnv, UserConfig, UserConfigExport, defineConfig } from 'vite';
 import { createHtmlPlugin } from 'vite-plugin-html';
+import { VitePWA } from 'vite-plugin-pwa';
 import svgr from 'vite-plugin-svgr';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
+export default defineConfig((({ mode }) => {
   const IS_PROD = mode === 'production';
 
   return {
@@ -15,6 +16,39 @@ export default defineConfig(({ mode }) => {
       tsconfigPaths(),
       svgr({ include: '**/*.svg?react' }),
       createHtmlPlugin({ minify: IS_PROD }),
+      VitePWA({
+        registerType: 'autoUpdate',
+        outDir: 'dist/',
+        devOptions: {
+          enabled: true,
+        },
+        manifest: {
+          name: 'Locus',
+          short_name: 'Locus',
+          description: 'Locus – приложение для организации совместного быта',
+          theme_color: '#ffffff',
+          display: 'standalone',
+          orientation: 'portrait',
+          start_url: '.',
+          icons: [
+            {
+              src: 'assets/favicons/android-chrome-192x192.png',
+              sizes: '192x192',
+              type: 'image/png',
+            },
+            {
+              src: 'assets/favicons/android-chrome-512x512.png',
+              sizes: '512x512',
+              type: 'image/png',
+            },
+            {
+              src: 'assets/favicons/apple-touch-icon.png',
+              sizes: '180x180',
+              type: 'image/png',
+            },
+          ],
+        },
+      }),
     ],
     server: {
       host: true,
@@ -27,4 +61,4 @@ export default defineConfig(({ mode }) => {
       postcss: { plugins: [autoprefixer()] },
     },
   };
-});
+}) as UserConfigExport);
